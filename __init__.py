e41dc21d69a5bd3471b3cb32374457df08c6178d
Giacomo Fragione <giacomo.fragione90@gmail.com>
2022-08-30 10:30:55 -0500

    code

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .imbhistory import __author__
+from .imbhistory import __email__
+from .imbhistory import __license__
+from .imbhistory import __version__
+from .imbhistory import __doc__
+
+from .imbhistory import imbhistory
